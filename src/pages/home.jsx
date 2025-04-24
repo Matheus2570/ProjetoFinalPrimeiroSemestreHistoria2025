9593a91ff9f36c266e9fea5ef4a275d610dab5bf
Matheus de Carvalho <matheusdecarvalho100@gmail.com>
2025-04-24 08:58:52 -0300

    Dia 24/04/2025

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "./home.css";
 import Botao from "../components/botao";
 
 function BuscaAutomatica() {
   const [dados, setDados] = useState([]);
-  const [erro, setErro] = useState('');
+  const [erro, setErro] = useState("");
   const [loading, setLoading] = useState(true);
 
   const termos = [
     "Guerra de Canudos",
     "Guerra do Contestado",
-    "Primeira Guerra Mundial"
+    "Primeira Guerra Mundial",
   ];
 
   useEffect(() => {
@@ -18,11 +18,12 @@ function BuscaAutomatica() {
       try {
         const resultados = [];
 
-
         await Promise.all(
           termos.map(async (termo) => {
             const res = await fetch(
-              `https://pt.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(termo)}`
+              `https://pt.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(
+                termo
+              )}`
             );
             const dado = await res.json();
             console.log(dado);
@@ -30,36 +31,40 @@ function BuscaAutomatica() {
           })
         );
 
-       
         localStorage.setItem("dadosHistoricos", JSON.stringify(resultados));
         setDados(resultados);
-
       } catch (erro) {
         setErro("Erro ao buscar dados.");
       } finally {
-        setLoading(false); 
+        setLoading(false);
       }
     };
 
     buscarDados();
-  }, []); 
+  }, []);
 
   return (
     <div className="container">
       <h1>Dados Históricos</h1>
-      {loading && <p>Carregando...</p>} 
+      {loading && <p>Carregando...</p>}
       {erro && <p style={{ color: "red" }}>{erro}</p>}
       {!loading && dados.length > 0 && (
         <div className="resultado">
           {dados.map((item, index) => (
             <div key={index} style={{ marginBottom: "30px" }}>
               <h2>{item.title}</h2>
-              <p><em>{item.description}</em></p>
+              <p>
+                <em>{item.description}</em>
+              </p>
               {item.thumbnail && (
                 <img src={item.thumbnail.source} alt={item.title} width={250} />
               )}
               <p>{item.extract}</p>
-              <a href={item.content_urls.desktop.page} target="_blank" rel="noreferrer">
+              <a
+                href={item.content_urls.desktop.page}
+                target="_blank"
+                rel="noreferrer"
+              >
                 Ver na Wikipedia
               </a>
             </div>
