4916a23c1711d53d25700f595df85a0e57fe0c87
TinaMartelli <valentinamartelli2008@gmail.com>
2025-05-15 11:40:44 -0300

    Valentina15/05/2025

--- src/pages/fascismoItaliano.jsx
+++ src/pages/fascismoItaliano.jsx
@@ -2,6 +2,32 @@ function Pagina5() {
   return (
     <div>
       <h1>Bem vindo</h1>
+
+      <p>
+        O fascismo Italiano foi um governo conservador e extremista que assumiu
+        o poder do governo por meio de Benito Mussolini em 1922 que acabou
+        oficialmente em 1943. O fascismo na Itália foi um período em que o país
+        foi governado por um partido político conservador, radical e de
+        extrema-direita, de 1922 a 1943, embora de alguma forma tenha se
+        sustentado no poder até 1945. O fascismo surgiu, em 1919, como uma
+        organização paramilitar, transformando-se em partido em 1921. Liderado
+        por Benito Mussolini, o fascismo se aproveitou do cenário de incerteza
+        na qual a Itália vivia após o trauma do pós-Primeira Guerra Mundial.
+        Usava da violência para alcançar os seus objetivos e cresceu
+        consideravelmente violentando os socialistas na Europa. Benito Mussolini
+        foi executado no final da Segunda Guerra Mundial. Diante disso o
+        fascismo via nessas práticas o objetivo único de alcançar o “destino
+        fatal de Roma”, que seria o de um país glorioso e dominador de muitas
+        terras. Inclusive, por conta disso, realizava um resgate do passado
+        histórico romano, com o objetivo de transmitir a ideia da “força” do
+        povo italiano
+      </p>
+
+      <p>
+Link: 
+https://brasilescola.uol.com.br/historiag/fascismo.htm
+
+</p>
     </div>
   );
 }
