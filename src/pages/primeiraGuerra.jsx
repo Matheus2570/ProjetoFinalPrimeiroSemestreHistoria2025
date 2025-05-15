a239d6a60952c3cd4f2be09aafd9338b96df1c56
Laura Marques <lauramarques00000@gmail.com>
2025-05-15 12:57:24 -0300

    Laura_15/05_modificação

--- src/pages/primeiraGuerra.jsx
+++ src/pages/primeiraGuerra.jsx
@@ -1,9 +1,120 @@
-function Pagina3() {
+import { useEffect, useState } from 'react';
+import SliderHome from '../components/sliderPaginas';
+import './cssPaginasGlobais.css';
+import Guerra1 from '../assets/PrimeiraGuerra1.png';
+import Guerra2 from '../assets/PrimeiraGuerra2.png';
+import Guerra3 from '../assets/PrimeiraGuerra3.png';
+import Guerra4 from '../assets/PrimeiraGuerra4.png';
+import Footer from '../components/footerPaginas.jsx';
+
+function Pagina4() {
+  const [dado, setDado] = useState(null);
+  const [erro, setErro] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const imagens = [
+    Guerra1,
+    Guerra2,
+    Guerra3,
+    Guerra4
+  ];
+
+  useEffect(() => {
+    const buscarDado = async () => {
+      try {
+        const dadoLocal = localStorage.getItem('PrimeiraGuerra');
+        if (dadoLocal) {
+          setDado(JSON.parse(dadoLocal));
+          setLoading(false);
+          return;
+        }
+
+        const res = await fetch(
+          `https://pt.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent("Primeira Guerra Mundial")}`
+        );
+        const resultado = await res.json();
+        localStorage.setItem("PrimeiraGuerra", JSON.stringify(resultado));
+        setDado(resultado);
+      } catch (erro) {
+        setErro("Erro ao buscar dados.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    buscarDado();
+  }, []);
+
   return (
-    <div>
-      <h1>Bem vindo</h1>
+    <div className="container">
+      {/* Bloco da API */}
+      {loading && <p>Carregando...</p>}
+      {erro && <p style={{ color: "red" }}>{erro}</p>}
+      {!loading && dado && (
+        <div className="api-section">
+          <h1>{dado.title}</h1>
+          <h3><em>{dado.description}</em></h3>
+          {dado.thumbnail && (
+            <img src={dado.thumbnail.source} alt={dado.title} width={250} />
+          )}
+          <p>{dado.extract}</p>
+          <a href={dado.content_urls.desktop.page} target="_blank" rel="noreferrer">
+            Ver na Wikipedia
+          </a>
+        </div>
+      )}
+
+      {/* Bloco com resumo + carrossel */}
+      <div className="resumo-pessoal">
+        <div className="resumo-texto">
+          <h2>Resumo pessoal – Primeira Guerra Mundial</h2>
+          <p>A Primeira Guerra Mundial (1914-1918) foi um conflito global entre a Tríplice Entente (França, Reino Unido, Rússia, depois Itália e EUA) e a Tríplice Aliança (Alemanha, Áustria-Hungria e Império Otomano).
+            As causas incluíram o imperialismo, o nacionalismo, a política de alianças,
+            a corrida armamentista e a instabilidade nos Balcãs, culminando no assassinato do arquiduque Francisco Ferdinando.
+            <br></br>
+            A guerra iniciou com a fase de movimento, mas logo se transformou em uma sangrenta guerra de trincheiras. Em 1917, a Rússia se retirou e os EUA entraram, mudando o equilíbrio de poder. As ofensivas finais
+            da Entente em 1918 levaram ao colapso das Potências Centrais.
+            <br></br>
+            As consequências foram vastas com milhões de mortos, o fim de impérios (Alemão, Austro-Húngaro, Russo e Otomano), a criação de novos países, um redesenho do mapa europeu, crise econômica,
+            a ascensão dos EUA, a criação da Liga das Nações e o punitivo Tratado de Versalhes, que semeou ressentimento na Alemanha e contribuiu para futuros conflitos. A guerra também gerou um profundo impacto social e cultural.</p>
+
+        </div>
+
+        <SliderHome imagens={imagens} />
+      </div>
+
+      {/* Bloco com os dois vídeos */}
+      <div className="video-section">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ZfZAle8CVgg?si=hfriiKYwrORaF64q"
+         title="YouTube video player"
+          frameborder="0"
+           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+           referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen>
+
+            </iframe>
+
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/0Cy2UIUsbus?si=o0FhVEU5AaI5aZVA" 
+       title="YouTube video player" 
+       frameborder="0" 
+       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+         allowfullscreen></iframe>
+      </div>
+
+      {/*Aqui*/}
+
+      <Footer
+        link1="https://brasilescola.uol.com.br/historiag/primeira-guerra.htm"
+        nome1="Site 1"
+        link2="https://www.todamateria.com.br/primeira-guerra-mundial/"
+        nome2="Site 2"
+        link3="https://mundoeducacao.uol.com.br/historiageral/primeira-guerra-mundial.htm"
+        nome3="Site 3"
+      />
+
     </div>
   );
 }
 
-export default Pagina3;
+export default Pagina4;
--- src/pages/revolucaoDe1930.jsx
+++ src/pages/revolucaoDe1930.jsx
@@ -1,9 +1,117 @@
-function Pagina7() {
+import { useEffect, useState } from 'react';
+import SliderHome from '../components/sliderPaginas';
+import './cssPaginasGlobais.css';
+import Revolucao1 from '../assets/Revolucao1930(1).png';
+import Revolucao2 from '../assets/Revolucao1930(2).png';
+import Revolucao3 from '../assets/Revolucao1930(3).png';
+import Revolucao4 from '../assets/Revolucao1930(4).png';
+import Footer from '../components/footerPaginas.jsx';
+
+function Pagina8() {
+  const [dado, setDado] = useState(null);
+  const [erro, setErro] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const imagens = [
+    Revolucao1,
+    Revolucao2,
+    Revolucao3,
+    Revolucao4
+  ];
+
+  useEffect(() => {
+    const buscarDado = async () => {
+      try {
+        const dadoLocal = localStorage.getItem('Revolucao1930');
+        if (dadoLocal) {
+          setDado(JSON.parse(dadoLocal));
+          setLoading(false);
+          return;
+        }
+
+        const res = await fetch(
+          `https://pt.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent("Revolução de 1930")}`
+        );
+        const resultado = await res.json();
+        localStorage.setItem("Revolucao1930", JSON.stringify(resultado));
+        setDado(resultado);
+      } catch (erro) {
+        setErro("Erro ao buscar dados.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    buscarDado();
+  }, []);
+
   return (
-    <div>
-      <h1>Bem vindo</h1>
+    <div className="container">
+      {/* Bloco da API */}
+      {loading && <p>Carregando...</p>}
+      {erro && <p style={{ color: "red" }}>{erro}</p>}
+      {!loading && dado && (
+        <div className="api-section">
+          <h1>{dado.title}</h1>
+          <h3><em>{dado.description}</em></h3>
+          {dado.thumbnail && (
+            <img src={dado.thumbnail.source} alt={dado.title} width={250} />
+          )}
+          <p>{dado.extract}</p>
+          <a href={dado.content_urls.desktop.page} target="_blank" rel="noreferrer">
+            Ver na Wikipedia
+          </a>
+        </div>
+      )}
+
+      {/* Bloco com resumo + carrossel */}
+      <div className="resumo-pessoal">
+        <div className="resumo-texto">
+          <h2>Resumo pessoal – Revolução de 1930</h2>
+          <p>A Revolução de 1930 foi um movimento político-militar que marcou o fim da República Velha e o início da Era Vargas no Brasil. Liderada por Getúlio Vargas, o então governador do Rio Grande do Sul, e com o apoio de outros estados como Minas Gerais e Paraíba, a revolução depôs o presidente Washington Luís antes que o presidente eleito Júlio Prestes tomasse posse.
+<br></br>
+A insatisfação com a política do "café com leite", que alternava o poder entre as oligarquias de Minas Gerais e São Paulo, a crise econômica de 1929 e o assassinato de João Pessoa, candidato a vice-presidente na chapa de Vargas pela Aliança Liberal, foram fatores cruciais para o desencadeamento do movimento.
+<br></br>
+Em outubro de 1930, as tropas revolucionárias iniciaram levantes em diversos estados, culminando na deposição de Washington Luís no dia 24 de outubro. Em 3 de novembro de 1930, Getúlio Vargas assumiu a chefia do Governo Provisório, marcando o fim da República Velha e o início de um novo período na história do Brasil, caracterizado pela centralização do poder e por importantes reformas sociais e econômicas.</p>
+
+        </div>
+
+        <SliderHome imagens={imagens} />
+      </div>
+
+      {/* Bloco com os dois vídeos */}
+      <div className="video-section">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/jQU6Ojetq8M?si=NblW6mIrHIDwrJgB"
+         title="YouTube video player"
+          frameborder="0"
+           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+           referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen>
+
+            </iframe>
+
+       <iframe
+       src="https://www.youtube.com/embed/z6SqhhdoCbc?si=x98T4I_YV-7arwhS" 
+       title="YouTube video player" 
+       frameborder="0" 
+       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+         allowfullscreen></iframe>
+      </div>
+
+      {/*Aqui*/}
+
+      <Footer
+        link1="https://mundoeducacao.uol.com.br/historiadobrasil/revolucao-1930.htm#:~:text=1930.%5B1%5D-,A%20Revolu%C3%A7%C3%A3o%20de%201930%20foi%20um%20movimento%20armado%2C%20liderado%20pelo,ga%C3%BAcho%20como%20l%C3%ADder%20de%20Estado."
+        nome1="Site 1"
+        link2="https://www.todamateria.com.br/revolucao-de-1930/"
+        nome2="Site 2"
+        link3="https://brasilescola.uol.com.br/o-que-e/historia/o-que-foi-revolucao-1930.htm"
+        nome3="Site 3"
+      />
+
     </div>
   );
 }
 
-export default Pagina7;
+export default Pagina8;
