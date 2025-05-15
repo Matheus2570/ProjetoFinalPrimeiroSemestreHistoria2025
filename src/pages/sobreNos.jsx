a8f5b8f8683168d288523fe8779f183bfd59a060
Ana Clara <anaclarazamonerdias@gmail.com>
2025-05-15 09:57:29 -0300

    Teste

--- src/pages/sobreNos.jsx
+++ src/pages/sobreNos.jsx
@@ -1,8 +1,36 @@
+import './sobreNos.css';
+
 function Pagina8() {
     return (
-      <div>
-        <h1>Bem vindo</h1>
-      </div>
+        <div>
+
+      <main>
+        <section className="grupo">
+          <img src="/grupo.jpg" alt="Foto do grupo" className='imgrupo'/>
+          <div className="nomes">
+            <p>
+              Matheus de Carvalho<br />
+              Maria Clara Cardoso Gomes Freire<br />
+              Laura Marques<br />
+              Ana Clara Zamoner Dias<br />
+              Vitor Gabriel de Paula<br />
+              Valentina Marcela Martelli
+            </p>
+          </div>
+        </section>
+
+        <section className="descricao">
+          <p>
+            Nós somos o grupo 5, grupo montado no ano de 2024 para trabalharmos juntos nas aulas do curso.
+            Agora com 1 ano e 5 meses de curso desenvolvemos esse site para auxiliar outros alunos assim como nós!
+            O site tem 7 conteúdos relacionados à disciplina de história com um vídeo produzido por nós exemplificando 
+            e aprofundando mais no conteúdo da Guerra de Contestado! Aproveitem! ❤️
+          </p>
+        </section>
+      </main>
+
+      
+    </div>
     );
   }
   
