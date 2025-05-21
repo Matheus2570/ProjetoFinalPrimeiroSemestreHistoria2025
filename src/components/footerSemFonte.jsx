942a2c2d3fce353d06ad1915d140b23fcd07d493
Matheus de Carvalho <matheusdecarvalho100@gmail.com>
2025-05-21 14:12:28 -0300

    Dia 15/05/2025

--- src/components/footerSemFonte.jsx
+++ src/components/footerSemFonte.jsx
@@ -4,33 +4,27 @@ function FooterSemFonte() {
   return (
     <>
       <footer className="Footer">
-        
-       
         <div className="Bloco3">
           <div className="BlocoSobreNos">
-            <p>
-              <div class="footer_social">
-                <a href="https://www.instagram.com" target="_blank">
-                  <i className="bx bxl-instagram"></i>
-                </a>
-                <a href="https://www.facebook.com" target="_blank">
-                  <i className="bx bxl-facebook"></i>
-                </a>
-                <a href="https://www.twitter.com" target="_blank">
-                  <i className="bx bxl-twitter"></i>
-                </a>
-              </div>
-            </p>
+        
+            <div className="footer_social">
+              <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                <i className="bx bxl-instagram"></i>
+              </a>
+              <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                <i className="bx bxl-facebook"></i>
+              </a>
+              <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+                <i className="bx bxl-twitter"></i>
+              </a>
+            </div>
+
             <p>Nós somos o grupo 5</p>
           </div>
-          <div className="BlocoReclamacoes">
-            <p>Numero para reclamações: 
-            (19) 9891-24968/ 0800-9875 
-            </p>
-            <p>Numero para feedbacks e dúvidas:
-            (19) 9891-24968/ 0800-9875 
 
-            </p>
+          <div className="BlocoReclamacoes">
+            <p>Numero para reclamações: (19) 9891-24968 / 0800-9875</p>
+            <p>Numero para feedbacks e dúvidas: (19) 9891-24968 / 0800-9875</p>
           </div>
         </div>
       </footer>
